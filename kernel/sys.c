52e71bca804b521b0c19f6a046dfb4b1eb4a867e
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2024-12-28 09:09:28 +0800

    Revert "kernel: bpf: Pretend to support ANY prog_type/attach_type"
    This reverts commit e7905dc87a056b8c3874a652647e0af610753d54.
    
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1215,7 +1215,6 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	susfs_spoof_uname(&tmp);
 #endif
-#ifdef CONFIG_ANDROID_SPOOF_KERNEL_VERSION_FOR_BPF
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
@@ -1223,7 +1222,6 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
-#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
