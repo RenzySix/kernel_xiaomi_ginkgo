e1a2d9c0b5917f8bb02d42dc3f206bd6e9f6d5ef
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2025-03-24 23:50:30 +0800

    build: switch to Clang 21.0
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- build-a11.sh
+++ build-a11.sh
@@ -30,7 +30,7 @@ export LOCALVERSION
 
 if ! [ -d "${CLANG_DIR}" ]; then
 echo "Clang not found! Cloning to ${TC_DIR}..."
-if ! git clone --depth=1 -b clang-20.0 https://gitlab.com/kutemeikito/rastamod69-clang ${CLANG_DIR}; then
+if ! git clone --depth=1 -b clang-21.0 https://gitlab.com/kutemeikito/rastamod69-clang ${CLANG_DIR}; then
 echo "Cloning failed! Aborting..."
 exit 1
 fi
