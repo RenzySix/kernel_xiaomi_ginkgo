17519c36786df23506a4cd3e928aea3c504d1722
Renzy <renzyxtreme@gmail.com>
2025-08-12 02:35:29 +0700

    Include Script

--- build.sh
+++ build.sh
@@ -10,9 +10,13 @@
 #
 # Use this script on root of kernel directory
 
+# Install required packages
+sudo apt-get update
+sudo apt-get install -y bc cpio ccache gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu build-essential flex bison libelf-dev libssl-dev python3 lld curl git
+
 SECONDS=0 # builtin bash timer
 DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M")
-ZIPNAME="Kazuya-Ginkgo-KSUNext-${DATE}.zip"
+ZIPNAME="Kazuya-Ginkgo-KSU-${DATE}.zip"
 TC_DIR="$HOME/toolchain"
 CLANG_DIR="${TC_DIR}/clang-r547379"
 ARCH_DIR="${TC_DIR}/aarch64-linux-android-4.9"
@@ -25,39 +29,39 @@ export LD_LIBRARY_PATH="$CLANG_DIR/lib:$LD_LIBRARY_PATH"
 export KBUILD_BUILD_VERSION="1"
 
 if ! [ -d "${CLANG_DIR}" ]; then
-echo "Clang not found! Cloning to ${TC_DIR}..."
-if ! git clone --depth=1 -b master https://gitlab.com/kei-space/clang/r547379 ${CLANG_DIR}; then
-echo "Cloning failed! Aborting..."
-exit 1
-fi
+  echo "Clang not found! Cloning to ${TC_DIR}..."
+  if ! git clone --depth=1 -b master https://gitlab.com/kei-space/clang/r547379 "${CLANG_DIR}"; then
+    echo "Cloning clang failed! Abort."
+    exit 1
+  fi
 fi
 
 if ! [ -d "${ARCH_DIR}" ]; then
-echo "gcc not found! Cloning to ${ARCH_DIR}..."
-if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git ${ARCH_DIR}; then
-echo "Cloning failed! Aborting..."
-exit 1
-fi
+  echo "gcc (aarch64) not found! Cloning..."
+  if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "${ARCH_DIR}"; then
+    echo "Cloning gcc aarch64 failed! Abort."
+    exit 1
+  fi
 fi
 
 if ! [ -d "${ARM_DIR}" ]; then
-echo "gcc_32 not found! Cloning to ${ARM_DIR}..."
-if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git ${ARM_DIR}; then
-echo "Cloning failed! Aborting..."
-exit 1
-fi
+  echo "gcc (arm 32-bit) not found! Cloning..."
+  if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git "${ARM_DIR}"; then
+    echo "Cloning gcc arm failed! Abort."
+    exit 1
+  fi
 fi
 
 if [[ $1 = "-k" || $1 = "--ksu" ]]; then
-echo -e "\nCleanup KernelSU first on local build\n"
-rm -rf KernelSU drivers/kernelsu
+  echo -e "\nCleanup KernelSU first on local build\n"
+    rm -rf KernelSU drivers/kernelsu
 
-echo -e "\nKSU Support, let's Make it On\n"
-curl -kLSs "https://raw.githubusercontent.com/Renzy16/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
+  echo -e "\nKSU Support, let's Make it On\n"
+    curl -kLSs "https://raw.githubusercontent.com/Renzy16/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
 
-sed -i 's/CONFIG_KSU=n/CONFIG_KSU=y/g' arch/arm64/configs/vendor/ginkgo_defconfig
+  sed -i 's/CONFIG_KSU=n/CONFIG_KSU=y/g' arch/arm64/configs/vendor/ginkgo_defconfig
 else
-echo -e "\nKSU not Support, let's Skip\n"
+  echo -e "\nKSU not Support, let's Skip\n"
 fi
 
 mkdir -p out
@@ -65,20 +69,19 @@ make O=out ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
 make -j$(nproc --all) O=out \
- ARCH=arm64 \
- CC=clang \
- LD=ld.lld \
- AR=llvm-ar \
- AS=llvm-as \
- NM=llvm-nm \
- OBJCOPY=llvm-objcopy \
- OBJDUMP=llvm-objdump \
- STRIP=llvm-strip \
- CROSS_COMPILE=${ARCH_DIR}/bin/aarch64-linux-gnu- \
- CROSS_COMPILE_ARM32=${ARM_DIR}/bin/arm-linux-gnueabi- \
- CLANG_TRIPLE=aarch64-linux-gnu- \
- Image.gz-dtb \
- dtbo.img
+  ARCH=arm64 \
+  CC=clang \
+  LD=ld.lld \
+  AR=llvm-ar \
+  AS=llvm-as \
+  NM=llvm-nm \
+  OBJCOPY=llvm-objcopy \
+  OBJDUMP=llvm-objdump \
+  STRIP=llvm-strip \
+  CROSS_COMPILE=${ARCH_DIR}/bin/aarch64-linux-gnu- \
+  CROSS_COMPILE_ARM32=${ARM_DIR}/bin/arm-linux-gnueabi- \
+  CLANG_TRIPLE=aarch64-linux-gnu- \
+  Image.gz-dtb dtbo.img
 
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
